cb1196202d414df92bee05fc3640a998aa3b76b0
Nestor <31547238+NestorRubio@users.noreply.github.com>
2023-04-25 10:51:02 -0600

    Debug = True

--- attendo_frontend_calificador/settings.py
+++ attendo_frontend_calificador/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2=_f(dlj6i(e1th#x(uh*9njnkxvp0@^*k8!75q*7&irje@232'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
